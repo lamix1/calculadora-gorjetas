d1d9d2fc291293b9fa6b6d725b1f358855817629
lamix1 <camilacristina328santoss@gmail.com>
2022-10-09 18:40:23 -0300

    Alteração

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,98 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      people: "",
+      count: "",
+      valor_gorjeta: "",
+    };
+  },
+  methods: {
+    calcular() {
+      const people = this.people;
+      const count = this.count;
+      const valor_gorjeta = this.valor_gorjeta;
+      let valor_valor_gorjeta = 0;
+      let resultado = {};
+      valor_valor_gorjeta = count * (valor_gorjeta / 100.0);
+      resultado = (count + valor_valor_gorjeta) / people;
+      document.getElementById("resultado").innerHTML = resultado;
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <body oi>
+    <main>
+      <div class="conteudo-oi">
+        <h1>Calculadora de gorjetas</h1>
+        <label>
+          <p>Qual o valor da conta?</p>
+          <input type="number" v-model="count" id="conta" />
+        </label>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <label>
+          <p>Qual foi a qualidade do serviço?</p>
+          <select oi v-model="valor_gorjeta">
+            <option value="30">30% (Excepcional)</option>
+            <option value="25">25% (Muito bom)</option>
+            <option value="20">20% (Bom)</option>
+            <option value="15">15% (Ok)</option>
+            <option value="10">10% (Ruim)</option>
+            <option value="5">5% (Péssimo)</option>
+          </select>
+        </label>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+        <label
+          ><p>Quantas pessoas estão dividindo a conta?</p>
+          <input type="number" v-model="people" id="people" />
+        </label>
+        <button type="button" @click="calcular">Calcular</button>
+        <hr />
+        <span id="resultado">Valor da Conta</span>
+      </div>
+    </main>
+  </body>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap");
+h1 {
+  color: #000000;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  font-size: 50px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+main {
+  background-color: #acd9ec;
+  padding: 60px;
+  border-radius: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+button {
+  background-color: #000000;
+  border: #1a5974;
+  color: white;
+  height: 30px;
+  width: 100px;
+  border-radius: 10px;
+  margin: 10px;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+input {
+  border-color: #1a5974;
+  border-radius: 20px;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+select {
+  border-radius: 30px;
+  border-color: #1a5974;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+h2 {
+  font-family: "Great Vibes", cursive;
+  font-size: 35px;
 }
 </style>
